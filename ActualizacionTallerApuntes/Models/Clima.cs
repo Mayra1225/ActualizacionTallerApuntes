00e652187f05e1b798b2087391fcc4b17f57602b
Mayra1225 <mayra.vargas@udla.edu.ec>
2025-06-23 15:23:02 -0500

    Creación de el Modelo y Servicio para la parte de Clima

---
+++ ActualizacionTallerApuntes/Models/Clima.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActualizacionTallerApuntes.Models
+{
+    public class Clima
+    {
+        public string Hora { get; set; }
+        public double Temperatura { get; set; }
+        public double Humedad { get; set; }
+        public double Lluvia { get; set; }
+    }
+}
---
+++ ActualizacionTallerApuntes/Services/ClimaService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ActualizacionTallerApuntes.Models;
+
+namespace ActualizacionTallerApuntes.Services
+{
+    public class ClimaService
+    {
+        private readonly HttpClient _http = new();
+
+        public async Task<Clima?> ObtenerClimaAsync(double lat, double lon)
+        {
+            string url = $"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,relative_humidity_2m,rain";
+            try
+            {
+                var json = await _http.GetStringAsync(url);
+                using var doc = JsonDocument.Parse(json);
+                var root = doc.RootElement.GetProperty("current");
+                return new Clima
+                {
+                    Hora = root.GetProperty("time").GetString(),
+                    Temperatura = root.GetProperty("temperature_2m").GetDouble(),
+                    Humedad = root.GetProperty("relative_humidity_2m").GetDouble(),
+                    Lluvia = root.GetProperty("rain").GetDouble()
+                };
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
